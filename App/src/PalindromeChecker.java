cda18a0e524162c676ba4726dfab737794574dfb
Abhishek Deep <deepabhishek50@gmail.com>
2026-02-19 11:15:44 +0530

    Refactor PalindromeChecker class structure

--- App/src/PalindromeChecker.java
+++ App/src/PalindromeChecker.java
@@ -1,5 +1,10 @@
-public class PalindromeCheckerApp {
+class PalindromeChecker {
+    /*
+        UC1
+    */
     public static void main(String[] args){
-        System.out.println("Welcome to Palindrome Checker App Management System");
+        System.out.println("Welcome to the Palindrome Checker Management System\n");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully");
     }
 }
